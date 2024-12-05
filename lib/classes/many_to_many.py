55c75e97481d08cf70d5c317ed735316feade753
Joyce Sheng <joycesheng96@hotmail.com>
2024-12-05 12:15:08 -0500

    idk how to get these tests to pass

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,37 +2,58 @@ class Article:
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self._title = title
+
+    @property
+    def title(self):
+        return self._title
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self._articles})
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        magazine._articles.append(article)
+        return article
 
     def topic_areas(self):
-        pass
+        return list({article.magazine.category for article in self._articles})
+
 
 class Magazine:
     def __init__(self, name, category):
+        if not (2 <= len(name) <= 16):
+            raise ValueError("Magazine name must be between 2 and 16 characters.")
+        if not category:
+            raise ValueError("Category must have a length greater than 0.")
         self.name = name
         self.category = category
+        self._articles = []
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list({article.author for article in self._articles})
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        pass
+        authors = {}
+        for article in self._articles:
+            authors[article.author] = authors.get(article.author, 0) + 1
+        return [author for author, count in authors.items() if count > 2]
